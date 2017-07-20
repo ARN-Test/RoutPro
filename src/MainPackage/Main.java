201449cf3b27e1292622347f4f2876c163f7b65e
ARN-Test <ashiqurs1234@gmail.com>
2017-07-20 12:21:36 +0600

    DB Add

--- src/MainPackage/Main.java
+++ src/MainPackage/Main.java
@@ -1,18 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package MainPackage;
+
+import java.sql.Connection;
 
 /**
  *
  * @author A.R. Nobel
- * @author Raihan Topu
  */
 public class Main {
+    public Connection ConnectDB;
+    
+    //Main - Constructor
+    public Main() {
+        this.ConnectDB = Connect.ConnectDB();
+    }
+    
+    //main - function
     public static void main(String[] args)
     {
         
     }
+
+    
 }
