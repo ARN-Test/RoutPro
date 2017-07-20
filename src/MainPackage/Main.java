a139c6d9014425fde8be59527389439cea265b8c
ARN-Test <ashiqurs1234@gmail.com>
2017-07-20 16:16:49 +0600

    CT-1.1

--- src/MainPackage/Main.java
+++ src/MainPackage/Main.java
@@ -1,23 +1,39 @@
 package MainPackage;
 
-import java.sql.Connection;
+import java.sql.*;
 
 /**
  *
  * @author A.R. Nobel
  */
 public class Main {
-    public Connection ConnectDB;
+    public static Connection CON = Connect.ConnectDB(); //For Connection in all Pages
     
-    //Main - Constructor
-    public Main() {
-        this.ConnectDB = Connect.ConnectDB();
-    }
     
     //main - function
     public static void main(String[] args)
     {
-        
+        try{
+        Statement ST = CON.createStatement();
+        ResultSet RS = ST.executeQuery("SELECT * FROM CLASS_LIST NATURAL JOIN TEACHER_LIST NATURAL JOIN ROOM_LIST");
+            while(RS.next())
+            {
+                System.out.println(RS.getString(1) + " " 
+                        + RS.getString(2) + " " 
+                        + RS.getString(3) + " " 
+                        + RS.getString(4) + " "
+                        + RS.getString(5) + " "
+                        + RS.getString(6) + " "
+                        + RS.getString(7) + " "
+                        + RS.getString(8) + " "
+                        + RS.getString(9) + " "
+                        + RS.getString(10) + " ");
+            }
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 
     
